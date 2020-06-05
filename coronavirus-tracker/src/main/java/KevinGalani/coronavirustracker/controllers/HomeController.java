4a0af309c048a8b5c4af8326d27e111231c5a607
Kevin <galani.kevin@gmail.com>
2020-06-04 22:03:25 -0400

    Adding Springboot project to track Coronavirus

---
+++ coronavirus-tracker/src/main/java/KevinGalani/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package KevinGalani.coronavirustracker.controllers;
+
+import KevinGalani.coronavirustracker.CoronaDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaDataService coronaDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("locationStats", coronaDataService.getAllStats());
+        return "home";
+    }
+
+}
